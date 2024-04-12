86edbc5b5c35745f4e6fc323a6632a5426904c4c
SangJee Park <sjpark3927@gmail.com>
2024-04-13 05:51:53 +0900

    commit2

--- main.js
+++ main.js
@@ -10,11 +10,27 @@
 // 2 true면 끝난 걸로 간주하고 밑줄 보여주기
 // 3 false 이면 안끝난 걸로 간주하고 그대로
 
-
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = []
+let filterList = []
+let mode='all'
+
 addButton.addEventListener("click", addTask)
+// 인풋값 초기화
+taskInput.addEventListener("click",function(){
+    taskInput.value=""; 
+});
+
+
+for(let i=1; i<tabs.length;i++){
+    tabs[i].addEventListener("click", function(event){
+        filter(event)
+    });
+
+}
+console.log(tabs)
 
 function addTask(){
     let task = {
@@ -28,22 +44,31 @@ function addTask(){
 }
 
 function render() {
+    // 1.내가 선택한 탭에 따라서
+    let list = []
+    if(mode == 'all'){
+        list = taskList;
+    } else if(mode == "ongoing" || mode === "done"){
+        list = filterList;
+    }
+    // 2. 리스트를 달리 보여준다. 
+
     let resultHTML = '';
-    for(let i = 0; i < taskList.length; i++) {
-        if(taskList[i].isComplete == true) {
+    for(let i = 0; i < list.length; i++) {
+        if(list[i].isComplete == true) {
             resultHTML += `<div class="task">
-            <div class="task-done">${taskList[i].taskContent}</div>
+            <div class="task-done">${list[i].taskContent}</div>
             <div>
-                <button onclick = "toggleComplete('${taskList[i].id}')">Check</button>
-                <button onclick = "deleteTask()">Delete</button>
+                <button onclick = "toggleComplete('${list[i].id}')">Check</button>
+                <button onclick = "deleteTask('${list[i].id}')">Delete</button>
             </div>
         </div>`;
         } else {
             resultHTML += `<div class="task">
-            <div>${taskList[i].taskContent}</div>
+            <div>${list[i].taskContent}</div>
                 <div>
-                    <button onclick = "toggleComplete('${taskList[i].id}')">Check</button>
-                    <button onclick = "deleteTask()">Delete</button>
+                    <button onclick = "toggleComplete('${list[i].id}')">Check</button>
+                    <button onclick = "deleteTask('${list[i].id}')">Delete</button>
                 </div>
             </div>`;
         }
@@ -64,10 +89,52 @@ function toggleComplete(id) {
      
 }
 
-function deleteTask(){
-    console.log("삭제")
+function deleteTask(id){
+    for(let i=0;i<taskList.length;i++){
+        if(taskList[i].id==id){
+            taskList.splice(i, 1)
+            break;
+        }
+    }
+    render();
+}
+
+function filter(event){
+    console.log("filter", event.target.id);
+    mode = event.target.id
+    filterList = []
+    if(mode === "all"){
+        //전체 리스트를 보여준다.
+        render()
+    } else if(mode === "ongoing"){
+        //진행 중이 아이템을 보여준다. 
+        // task.isComplete=false
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete === false){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+        console.log('진행', filterList)
+    } else if(mode==="done"){
+        // 끝나는 케이스
+        // task.isComplete=true
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete === true){
+                filterList.push(taskList[i]);
+            }
+        }
+        render()
+    }
 }
 
 function randomIDGenerate(){
     return '_' + Math.random().toString(36).substr(2, 9);
 }
+
+// 인풋값이 없을 경우 버튼 사용 불가
+// if(taskInput.value == ""){
+//     addButton.disabled = true;
+// } else {
+//     addButton.disabled = false;
+// }
